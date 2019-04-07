81885dd2570b9a877d7f3f2fbd77387af5e3bdad
AbdelkarimELKHATIB <abdelkarim.elkhatib@gmail.com>
2019-04-07 14:59:33 +0200

    first commit

---
+++ src/main/java/com/sqli/NlpRestVer2Application.java
@@ -0,0 +1,13 @@
+package com.sqli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpRestVer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpRestVer2Application.class, args);
+	}
+
+}
