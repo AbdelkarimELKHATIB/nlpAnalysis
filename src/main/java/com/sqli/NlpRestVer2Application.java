aacce6c378cd059de4d11bd3d78b0fd1eca86ffb
AbdelkarimELKHATIB <abdelkarim.elkhatib@gmail.com>
2019-04-09 00:45:43 +0200

    first commit

--- src/main/java/com/sqli/NlpRestVer2Application.java
+++ src/main/java/com/sqli/NlpRestVer2Application.java
@@ -2,8 +2,10 @@ package com.sqli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.core","com.controller","com.model"})
 public class NlpRestVer2Application {
 
 	public static void main(String[] args) {
